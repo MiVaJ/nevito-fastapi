b8c5baec73c44cd56a4410b13a5fdca31ccdf14f
MiVaJ <mylerysik@gmail.com>
2026-04-08 17:13:23 +0700

    feat: update user listing and add email validation

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Path, Query
+from fastapi import Body, FastAPI, HTTPException, Path, Query, status
 
 app = FastAPI(
     openapi_tags=[
         {
             "name": "Домашнее задание от 28.03.2026",
-            "description": "Валидация ID пользователя и Фильтрация товаров с валидацией",
+            "description": "Валидация и фильтрация",
+        },
+        {
+            "name": "Домашнее задание от 29.03.2026",
+            "description": "Работа с пользователями: создание, получение списка",
         },
     ]
 )
@@ -32,6 +36,15 @@ async def calculate(
         return {"error": f"Неизвестная операция: {operation}"}
 
 
+users_db = {
+    1: {"name": "Ivan", "age": 25, "email": "ivan@example.com"},
+    2: {"name": "Alice", "age": 30, "email": "alice@example.com"},
+    3: {"name": "John", "age": 22, "email": "john@example.com"},
+    4: {"name": "Sophia", "age": 28, "email": "sophia@example.com"},
+    5: {"name": "Michael", "age": 35, "email": "michael@example.com"},
+}
+
+
 @app.get("/users/{user_id}", tags=["Домашнее задание от 28.03.2026"])
 async def get_user(
     user_id: Annotated[int, Path(description="ID пользователя от 1 до 1000")],
@@ -41,6 +54,36 @@ async def get_user(
     if user_id > 1000:
         return {"detail": "ID должен быть от 1 до 1000"}
     return {"user_id": user_id, "name": "Иван"}
+
+
+@app.get("/users", tags=["Домашнее задание от 29.03.2026"])
+async def get_users() -> list[dict]:
+    # Преобразуем словарь users_db в список, добавляя ID в каждый объект пользователя
+    return [{"id": key} | value for key, value in users_db.items()]
+
+
+@app.post(
+    "/users",
+    status_code=status.HTTP_201_CREATED,
+    tags=["Домашнее задание от 29.03.2026"],
+)
+async def create_user(
+    name: Annotated[str, Body(max_length=20, description="Имя пользователя")],
+    age: Annotated[
+        int, Body(ge=1, le=120, description="Возраст пользователя (от 1 до 120 лет)")
+    ],
+    email: Annotated[str, Body(max_length=50, description="Электронная почта")],
+) -> dict:
+    for user in users_db.values():
+        if user["email"] == email:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="Email already exists"
+            )
+
+    new_index = max(users_db) + 1 if users_db else 1
+    new_user = {"name": name, "age": age, "email": email}
+    users_db[new_index] = new_user
+    return new_user
 
 
 @app.get("/products", tags=["Домашнее задание от 28.03.2026"])
