ede21737cff83318f7243719b83957e02eb8ea1b
MiVaJ <mylerysik@gmail.com>
2026-05-01 19:19:49 +0700

    feat: init react application and setup basic layout

--- backend/main.py
+++ backend/main.py
@@ -41,7 +41,7 @@ app = FastAPI(
 # Настройка CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["null"],
+    allow_origins=["null", "http://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
